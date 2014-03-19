a78f4bb6c3d12e4b97da04f08a2ce8df75a020be
Joseph McCarron <joe@josephmccarron.com>
2014-03-19 01:56:37 -0700

    build parsing logic and more

--- app.js
+++ app.js
@@ -22,18 +22,26 @@
           data: query.payload
         };
       },
-      getTicket: function () {
-        //use this only after updating the ticket asynchronously
-        return {
-          url: helpers.fmt('/api/v2/tickets/%@.json',this.ticket().id()),
-          type: 'GET'
-        };
-      },
-      getTicketAudits: function () {
-        return {
-          url: helpers.fmt('/api/v2/tickets/%@/audits.json',this.ticket().id()),
-          type: 'GET'
-        };
+      // getTicket: function () {
+      //   //use this only after updating the ticket asynchronously
+      //   return {
+      //     url: helpers.fmt('/api/v2/tickets/%@.json',this.ticket().id()),
+      //     type: 'GET'
+      //   };
+      // },
+      getTicketAudits: function (page) {
+        if(page) {
+          return {
+            url: helpers.fmt('/api/v2/tickets/%@/audits.json?page=%@', this.ticket().id(), page),
+            type: 'GET'
+          };
+        } else {
+          return {
+            url: helpers.fmt('/api/v2/tickets/%@/audits.json', this.ticket().id()),
+            type: 'GET'
+          };
+        }
+        
       },
       updateTicket: function (payload) {
         return {
@@ -55,6 +63,9 @@
         this.switchTo('form');
       } else if (currentLocation == 'nav_bar') {
         this.switchTo('search');
+        var daysBack = this.setting('days_back');
+        this.$('.start_date').datepicker().datepicker("setDate", new Date(new Date().setDate(new Date().getDate() - daysBack)));
+        this.$('.end_date').datepicker().datepicker("setDate", new Date());
       }
     },
     loadTime: function() {
@@ -135,18 +146,70 @@
     },
     getLogsFromTickets: function() {
       var ticket = this.ticket();
-      this.ajax('getTicketAudits').done( function (data) {
-        //parse ticketAudits into an object containing sum amounts of time given the specified date range
-        var ticketAudits = data.audits;
-        _.each(ticketAudits, function(audit) {
-          _.each(audit.events, function(event){
-            // TODO: check events for field_names matching the fields specified in settings
-              //...if true: grab the info and process it
-            console.log(event.field_name);
-          });
-        });
-    
+      this.ticketAudits = [];
+      this.ajax('getTicketAudits');
+    },
+    parseAudits: function (data) {
+      this.ticketAudits = data.audits.concat(ticketAudits);
+      if(data.next_page) {
+        this.ajax('getTicketAudits', data.next_page);
+      } else {
+        var total_time_field = this.setting('total_time_field_id'),
+          billable_time_field = this.setting('billable_time_field_id'),
+          external_time_field = this.setting('external_time_field_id');
+
+      var getDelta = function(event) {
+        var delta = event.value - event.previous_value;
+        return delta;
+      };
+
+      var total_time_entries = [],
+        billable_time_entries = [],
+        external_time_entries = [],
+        event_entries = [];
+
+      _.each(ticketAudits, function(audit) {
+        var date_field = this.setting('date_field_id'),
+          start_date = Date.parse(this.$('.start_date').val()),
+          end_date = Date.parse(this.$('.end_date').val()),
+          
+          date_field_event = _.where(audit.events, {field_name: date_field}),
+          date_value = Date.parse(date_field_event.value);
+
+        if(date_value >= start_date && date_value <= end_date) {
+        //IF the date field value in this audit is between the start and end dates...
+        //TODO refactor: if this works without error change to findWhere for efficiency
+          var total_time_event = _.where(audit.events, {field_name: total_time_field}),
+            billable_time_event = _.where(audit.events, {field_name: billable_time_field}),
+            external_time_event = _.where(audit.events, {field_name: external_time_field});
+
+          var total_delta = getDelta(total_time_event),
+            billable_delta = getDelta(billable_time_event),
+            external_delta = getDelta(external_time_event);
+
+          var compound_entry = {
+            'total_time': total_delta,
+            'billable_time': billable_delta,
+            'external_time': external_delta,
+            'date': date_value
+          };
+
+          total_time_entries.push(total_delta);
+          billable_time_entries.push(billable_delta);
+          external_time_entries.push(external_delta);
+          event_entries.push(compound_entry);
+        }
       });
+
+
+
+
+      }
+      //parse ticketAudits into an object containing sum amounts of time given the specified date range
+      
+      
+      
+      
     },
     // ##Helpers
     totalTimeFieldLabel: function() {
