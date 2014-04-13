d377b5ea784745284bd5677328377e200ca9d113
Joseph <joe@josephmccarron.com>
2014-04-13 01:19:59 -0700

    admin improvements

--- app.js
+++ app.js
@@ -7,9 +7,13 @@
       'click .default':'loadTime',
       'click .cancel_button':'loadDefault',
       'click .log_time':'buildLog',
+      'updateTicket.done':'onTicketUpdateSuccess',
+
       'keyup #input':'onSearchChanged',
+      'getOrgsAuto.done':'handleOrgs',
+      'click .org':'handleOrgSelection',
       'click .search':'getListOfTickets',
-      'updateTicket.done':'onTicketUpdateSuccess'
+
       // 'getTicketAuditsByID.done':'storeAudits',
       // 'getTicketAuditsByPage.done':'storeAudits'
       //'ticket.custom_field_{{total_time_field_id}}':'loadTime'
@@ -67,14 +71,18 @@
     loadDefault: function() {
       var currentLocation = this.currentLocation();
       console.log("Location: " + currentLocation);
+      var daysBack = this.setting('days_back');
       if (currentLocation == 'ticket_sidebar') {
         this.disableFields();
         this.switchTo('form');
+        this.$('.date').datepicker().datepicker("setDate", new Date());
       } else if (currentLocation == 'nav_bar') {
+        console.log("Detected nav bar location");
         this.switchTo('search');
-        var daysBack = this.setting('days_back');
-        this.$('.start_date').datepicker().datepicker("setDate", new Date(new Date().setDate(new Date().getDate() - daysBack)));
-        this.$('.end_date').datepicker().datepicker("setDate", new Date());
+        var start_date = new Date(new Date().setDate(new Date().getDate() - daysBack));
+        this.$('.start_date').datepicker().datepicker("setDate", start_date);
+        this.$('.end_date').datepicker();
+        this.$('.end_date').datepicker("setDate", new Date());
       }
     },
     loadTime: function() {
@@ -99,14 +107,14 @@
         log.external_time = log.units;
       } else {
         log.external = false;
-        log.internal_time = log.units;
+        log.external_time = 0;
       }
       if (this.$("select.billable").val() == "billable") {
         log.billable = true;
         log.billable_time = log.units;
       } else {
         log.billable = false;
-        log.non_billable_time = log.units;
+        log.billable_time = 0;
       }
       log.date = this.$("input.date").val();
       this.buildUpdate(log);
@@ -114,9 +122,9 @@
     buildUpdate: function(log) {
       var tkt = this.ticket(),
         id = tkt.id(),
-        total_time = ( log.units + parseFloat(tkt.customField(this.totalTimeFieldLabel()), 10))|| 0,
-        billable_time = ( log.billable_time + parseFloat(tkt.customField(this.billableTimeFieldLabel()), 10))|| 0,
-        external_time = ( log.external_time + parseFloat(tkt.customField(this.externalTimeFieldLabel()), 10))|| 0,
+        total_time = ( log.units + parseFloat(tkt.customField(this.totalTimeFieldLabel()), 10)),
+        billable_time = ( log.billable_time + parseFloat(tkt.customField(this.billableTimeFieldLabel()), 10)),
+        external_time = ( log.external_time + parseFloat(tkt.customField(this.externalTimeFieldLabel()), 10)),
         update = {"ticket":
         {"custom_fields":[
           {
@@ -151,53 +159,75 @@
           "string":name,
           "payload":"\"{'name': '" + name + "'}\""
         };
-        this.ajax('getOrgsAuto',query).done(function(response) {
-
-          //we need to render the organization options and allow the user to select one
-        });
+        this.ajax('getOrgsAuto',query);
+      } else {
+        //this.$("div.org_results").html("");
       }
     },
-    getListOfTickets: function() {
-      var org_id = this.$('input.search_field').val();
-      //console.log(org_id);
+    handleOrgs: function (response) {
+      var orgs = response.organizations;
+      var html = this.renderTemplate('organizations', {
+        orgs: orgs
+      });
+      //console.log(html);
+      //console.log(this.$("div.results").html());
+      this.$("div.org_results").html(html);
+    },
+    handleOrgSelection: function(e) {
+      if (e) {e.preventDefault();}
+      // console.log(e.currentTarget.value);
+      var org_id = e.currentTarget.value;
+      this.org_name = e.currentTarget.innerHTML;
+      //this.ajax('getOrgTickets', this.org_id);
+      this.$('input.search_field').val(this.org_id);
+      this.getListOfTickets(org_id);
+    },
+    getListOfTickets: function(org_id) {
+      //var org_id = this.$('input.search_field').val();
+      console.log(org_id);
       var tickets = this.paginate({request : 'getOrgTickets',
                               entity  : 'tickets',
                               id      : org_id,
                               page    : 1 });
       tickets.done(_.bind(function(tkts){
+
+
         this.getLogsFromTickets(tkts);
       }, this));
     },
     getLogsFromTickets: function(tkts) {
-      this.ticketAudits = [];
+      this.ticketsWithLogs = {};
 
       //loop through each ticket in the array
       _.each(tkts, function (tkt) {
-        this.ajax('getTicketAudits', tkt.id, 1);
-        var tkt_id = tkt.id;
+        // this.ajax('getTicketAudits', tkt.id, 1);
+        var tkt_id = tkt.id.toString();
         var audits = this.paginate({request : 'getTicketAudits',
                               entity  : 'audits',
                               id      : tkt_id,
                               page    : 1 });
         audits.done(_.bind(function(audits){
-          this.parseAudits(audits);
+          this.ticketsWithLogs.tkt_id = this.parseAudits(audits);
+          // console.log("Calling parse audits for ticket " + tkt_id);
+          console.log(this.ticketsWithLogs.tkt_id);
         }, this));
-      }.bind(this));
 
+      }.bind(this));
+      console.log(this.ticketsWithLogs);
       
     },
-    storeAudits: function (data, end) {
-      var ticketAudits = data.audits;
-      this.ticketAudits = this.ticketAudits.concat(ticketAudits);
-      if(data.next_page) {
-        this.ajax('getTicketAuditsByPage', data.next_page);
-      } else {
-        if (end) {
-          //end isn't being sent yet
-          this.parseAudits();
-        }
-      }
-    },
+    // storeAudits: function (data, end) {
+    //   var ticketAudits = data.audits;
+    //   this.ticketAudits = this.ticketAudits.concat(ticketAudits);
+    //   if(data.next_page) {
+    //     this.ajax('getTicketAuditsByPage', data.next_page);
+    //   } else {
+    //     if (end) {
+    //       //end isn't being sent yet
+    //       this.parseAudits();
+    //     }
+    //   }
+    // },
     parseAudits: function (audits) {
       var total_time_field = this.setting('total_time_field_id').toString(),
         billable_time_field = this.setting('billable_time_field_id').toString(),
@@ -216,43 +246,71 @@
       var date_field = this.setting('date_field_id').toString(),
         start_date = Date.parse(this.$('.start_date').val()),
         end_date = Date.parse(this.$('.end_date').val());
-      //console.log(audits);
+      // console.log(audits);
       //the stage is set, now iterate over all the audits, grab those in range, and build them into logs  
       _.each(audits, function(audit) {
-        var date_field_event = _.filter(audit.events, function(event){ return event.field_name === date_field ;}),//_.where(audit.events, {field_name: date_field}),
-          date_value = Date.parse(date_field_event.value);
-        console.log("Start date: " + start_date);
-        console.log("Date value: " + date_value);
-        console.log("End date: " + end_date);
-        if(date_value >= start_date && date_value <= end_date) {
-        //IF the date field value in this audit is between the start and end dates...
-          console.log(date_field_event);
-          var total_time_event = _.filter(audit.events, function(event){ return event.field_name === total_time_field ;}), //_.where(audit.events, {field_name: total_time_field}),
-            billable_time_event = _.filter(audit.events, function(event){ return event.field_name === billable_time_field ;}), //_.where(audit.events, {field_name: billable_time_field}),
-            external_time_event = _.filter(audit.events, function(event){ return event.field_name === external_time_field ;}); //_.where(audit.events, {field_name: external_time_field});
+        var date_field_event = _.filter(audit.events, function(event){ return event.field_name == date_field ;});//_.where(audit.events, {field_name: date_field}),
+        if(date_field_event[0]) {
+          var date_value = Date.parse(date_field_event[0].value);
+          // console.log("Start date: " + start_date);
+          // console.log("Date value: " + date_value);
+          // console.log("End date:   " + end_date);
+          if(date_value >= start_date && date_value <= end_date) {
+          //IF the date field value in this audit is between the start and end dates...
+            // console.log(date_field_event);
+            var total_time_event = _.filter(audit.events, function(event){ return event.field_name == total_time_field ;}), //_.where(audit.events, {field_name: total_time_field}),
+              billable_time_event = _.filter(audit.events, function(event){ return event.field_name == billable_time_field ;}), //_.where(audit.events, {field_name: billable_time_field}),
+              external_time_event = _.filter(audit.events, function(event){ return event.field_name == external_time_field ;}); //_.where(audit.events, {field_name: external_time_field});
 
-          var total_delta = getDelta(total_time_event),
-            billable_delta = getDelta(billable_time_event),
-            external_delta = getDelta(external_time_event);
+            //IF the event exists
+            var total_delta,
+              billable_delta,
+              external_delta;
+            if (total_time_event[0]) {
+              total_delta = getDelta(total_time_event[0]);
+              // console.log(total_time_event[0]);
+            } else {
+              total_delta = 0;
+            }
+            if (billable_time_event[0]) {
+              billable_delta = getDelta(billable_time_event[0]);
+              // console.log(billable_time_event[0]);
+            } else {
+              billable_delta = 0;
+            }
+            if (external_time_event[0]) {
+              external_delta = getDelta(external_time_event[0]);
+              // console.log(external_time_event[0]);
+            } else {
+              external_delta = 0;
+            }
+            //var total_delta = getDelta(total_time_event[0]) || 0,
+              // billable_delta = getDelta(billable_time_event[0]) || 0,
+              // external_delta = getDelta(external_time_event[0]) || 0;
 
-          var compound_entry = {
-            'total_time': total_delta,
-            'billable_time': billable_delta,
-            'external_time': external_delta,
-            'date': date_value
-          };
+            var compound_entry = {
+              'total_time': total_delta,
+              'billable_time': billable_delta,
+              'external_time': external_delta,
+              'date': date_value
+            };
 
-          console.log(compound_entry);
-          total_time_entries.push(total_delta);
-          billable_time_entries.push(billable_delta);
-          external_time_entries.push(external_delta);
-          event_entries.push(compound_entry);
+            // console.log(compound_entry);
+            total_time_entries.push(total_delta);
+            billable_time_entries.push(billable_delta);
+            external_time_entries.push(external_delta);
+            event_entries.push(compound_entry);
+          }
         }
+
       });
-      //console.log(event_entries);
+      // console.log(event_entries);
+      return event_entries;
+      //push event entries from each ticket to an object containing all the tickets in range, and their entries
     },
     onTicketUpdateSuccess: function() {
       services.notify("Ticket successfully updated with time log. Refresh to see changes.");
+      console.log("Ticket successfully updated.");
     },
 
     // ##Helpers
